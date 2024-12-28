e3e4e3ae1fc32828e54dc45040f9752c5af5e5e6
Vaibhav Khatri <20cs1132@mitsgwl.ac.in>
2024-12-28 11:53:05 +0530

    font weight of log out

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -237,7 +237,7 @@ const NavBar = () => {
             </NavLink>
             <a
               href="#"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              className="flex items-center p-2 font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img src={one} />
               <span className="ms-3 text-[#5F6165]">Log Out</span>
